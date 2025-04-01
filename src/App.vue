2f9cb29bd9046d767efcf9f63b418324e924b4f7
Yoke <359759651@qq.com>
2025-04-01 13:48:42 +0800

    测试文件

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
-<script setup lang="ts">
-
-</script>
-
 <template> </template>
+
+<script setup lang="ts">
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  const fun = (arr) => {
+    for (let index = 0; index < arr.length; index++) {
+      const element = arr[index]
+      console.log(element)
+    }
+  }
+  fun(arr)
+</script>
 
 <style scoped>
 
