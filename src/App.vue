88cde2536ec1e0dfcce19a38d217ee55e3aa6425
Yoke <359759651@qq.com>
2025-04-01 18:00:03 +0800

    测试文件

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 
 <script setup lang="ts">
   const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-  const fun = (arr) => {
+  const fun = (arr: any) => {
     for (let index = 0; index < arr.length; index++) {
       const element = arr[index]
       console.log(element)
--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,15 @@ import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
-
-
+const arr = [1,2,3,4,5,6,7,8,9]
+  const fun = (arr:any) => {
+    for (let index = 
+    0; index < 
+    arr.length; 
+    index++) {
+      const element 
+      = arr[index]
+      console.log(element)
+    }
+  }
+  fun(arr)
